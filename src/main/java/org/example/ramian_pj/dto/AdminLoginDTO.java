4f0acae8c5b947059f2c95b0e575b5a18cefd1a3
sagong0 <1w2q3e@naver.com>
2025-04-25 11:27:59 +0900

    Lombok 의존성 추가 및 AdminLoginDTO 생성

--- src/main/java/org/example/ramian_pj/dto/AdminLoginDTO.java
+++ src/main/java/org/example/ramian_pj/dto/AdminLoginDTO.java
@@ -1,5 +1,11 @@
 package org.example.ramian_pj.dto;
 
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
 public class AdminLoginDTO {
     private String aid;
     private String apw;
