d0776fef0c23f9edffce3bef7e869a3abee448c6
sagong0 <1w2q3e@naver.com>
2025-05-12 15:12:21 +0900

    ClientMember Domain 추가

---
+++ src/main/java/org/example/ramian_pj/domain/ClientMember.java
@@ -0,0 +1,29 @@
+package org.example.ramian_pj.domain;
+
+import java.time.LocalDateTime;
+
+public class ClientMember {
+    private Long id;
+    // 아이디
+    private String userid;
+    // 이름
+    private String username;
+    // 비번
+    private String pw;
+    // 휴대번호
+    private String phoneNumber;
+    // 휴대번호 인증 여부
+    private Boolean phoneVerified;
+    // 이메일
+    private String email;
+    // 우편번호
+    private String zipcode;
+    // 도로명 주소
+    private String streetAddress;
+    // 상세주소
+    private String detailAddress;
+    // 생성일자
+    private LocalDateTime createdAt;
+    // 최종 수정일자
+    private LocalDateTime updatedAt;
+}
