9d3b8f6a78bdf08b0ce14dfde773edd30d13268b
sagong0 <1w2q3e@naver.com>
2025-06-16 15:49:17 +0900

    SearchType 추가 - 화이트리스트 검증용

---
+++ src/main/java/org/example/ramian_pj/domain/SearchType.java
@@ -0,0 +1,14 @@
+package org.example.ramian_pj.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public interface SearchType {
+    String USERNAME = "username";
+    String USERID = "userid";
+    String USER_TELL = "phone_number";
+
+    static List<String> values() {
+        return Arrays.asList(USERNAME, USERID, USER_TELL);
+    }
+}
