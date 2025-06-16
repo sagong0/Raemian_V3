25beb2dae01e4594710719fd3246c6a5d955b6f9
sagong0 <1w2q3e@naver.com>
2025-06-16 15:50:31 +0900

    검색조건DTO 추가

---
+++ src/main/java/org/example/ramian_pj/dto/SearchConditionDTO.java
@@ -0,0 +1,22 @@
+package org.example.ramian_pj.dto;
+
+
+import lombok.Data;
+
+@Data
+public class SearchConditionDTO {
+    private String keyword;
+
+    // username, userid, user_tell
+    private String searchType;
+
+    private String sortBy = "userid";
+
+    private String order = "asc";
+    
+    private int page = 1;
+    
+    // 한페이지당 출력 데이터 수
+    private int pageSize = 5;
+    
+}
