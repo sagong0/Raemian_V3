290f57b09cb540a19014ccfd1e98bf8f9373685c
sagong0 <1w2q3e@naver.com>
2025-07-11 15:44:53 +0900

    Notice Upload 파일 있을경우, 없을 경우 로직만 작성 --> CDN서버 구축 전으로 임시 경로 사용중 추후 변경 필요 !! , mybatis query작성 해야 할 것

--- src/main/java/org/example/ramian_pj/controller/AdminController.java
+++ src/main/java/org/example/ramian_pj/controller/AdminController.java
@@ -3,6 +3,7 @@ package org.example.ramian_pj.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.ramian_pj.dto.*;
 import org.example.ramian_pj.service.AdminService;
+import org.example.ramian_pj.service.NoticeService;
 import org.example.ramian_pj.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +16,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RequestMapping("/admin")
@@ -27,6 +31,7 @@ public class AdminController {
 
     private final AdminService adminService;
     private final UserService userService;
+    private final NoticeService noticeService;
 
     @GetMapping({"", "/"})
     public String loginPage() {
@@ -149,19 +154,48 @@ public class AdminController {
     public String noticeWrite(
             @ModelAttribute NoticeDTO noticeDTO
     ) {
-        log.info("제목: {}", noticeDTO.getNtitle());
-        log.info("작성자: {}", noticeDTO.getNwriter());
-        log.info("내용: {}", noticeDTO.getNcontent());
 
-        if (noticeDTO.getNfile() != null && noticeDTO.getNfile().isEmpty()) {
-            log.info("첨부파일: {}", noticeDTO.getNfile().getOriginalFilename());
-            // 파일 저장 로직 수행
+        MultipartFile file = noticeDTO.getNfile();
+
+        // 파일 저장위치 TODO -> CDN 경로 수정 필요
+        String uploadDir = "D:/temp/";
+
+        try{
+            // 1) 공지사항 먼저 일단 저장 -> notice_id 생성위해 ( 외래키 )
+            log.info("noticeDTO = ", noticeDTO);
+            noticeService.saveNotice(noticeDTO);
+
+            // 2) 파일이 존재 할 경우만 처리
+            if(file != null && !file.isEmpty()){
+                String originalFileName = file.getOriginalFilename();
+                String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
+
+                String savedFileName = UUID.randomUUID().toString() + "_" + extension;
+
+                // 저장 될 경로 지정
+                File dest = new File(uploadDir + savedFileName);
+                // 해당 경로에 저장
+                file.transferTo(dest);
+
+                NoticeFileDTO fileDTO = new NoticeFileDTO();
+                fileDTO.setNoticeId(noticeDTO.getNoticeId());
+                fileDTO.setFileName(savedFileName);
+                fileDTO.setFilePath("/upload/notice/" + savedFileName);
+                fileDTO.setFileSize(file.getSize());
+                fileDTO.setContentType(file.getContentType());
+
+                // DB 저장
+                noticeService.saveNoticeFile(fileDTO);
+
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+            return "redirect:/admin/notice/write?error=true";
         }
 
-        // DB 저장 처리 ( 파일 첨부 없는 상황 )
-        log.info("파일 첨부없네 저장하자 ~ ");
-//        adminService.saveNotice(noticeDTO);
-        return "";
+
+        return "redirect:/admin/notice";
     }
 
 }
