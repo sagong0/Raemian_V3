327ce061e9c85949c4bb74e2c6389e1d432fe20b
sagong0 <1w2q3e@naver.com>
2025-07-28 14:26:52 +0900

    /reserve 경로에 interceptor 적용 및 구현

---
+++ src/main/java/org/example/ramian_pj/conf/LoginCheckInterceptor.java
@@ -0,0 +1,24 @@
+package org.example.ramian_pj.conf;
+
+import org.example.ramian_pj.dto.UserJoinDTO;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        UserJoinDTO user = (UserJoinDTO) session.getAttribute("user");
+
+        if(user == null){
+            response.sendRedirect(request.getContextPath() + "/reserve");
+            return false;
+        }
+
+        return true;
+    }
+}
