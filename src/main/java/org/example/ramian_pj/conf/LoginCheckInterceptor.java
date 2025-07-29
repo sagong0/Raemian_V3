76363d280fa20d29fe8b8a9249072a2bec2f10da
sagong0 <1w2q3e@naver.com>
2025-07-29 15:43:27 +0900

    LoginCheckInterceptor 수정(header.jsp에 세셥 기반 로직 추가)

--- src/main/java/org/example/ramian_pj/conf/LoginCheckInterceptor.java
+++ src/main/java/org/example/ramian_pj/conf/LoginCheckInterceptor.java
@@ -15,7 +15,8 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
         UserJoinDTO user = (UserJoinDTO) session.getAttribute("user");
 
         if(user == null){
-            response.sendRedirect(request.getContextPath() + "/");
+            session.setAttribute("msg","로그인 후 이용 가능한 서비스입니다.");
+            response.sendRedirect(request.getContextPath() + "/login");
             return false;
         }
 
